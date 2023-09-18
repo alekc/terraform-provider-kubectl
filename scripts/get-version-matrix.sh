5b64c7355f37965e3f0f20792d7531e340d70c7f
Alexander Chernov <alexander@chernov.it>
2023-09-18 15:47:51 +0100

    chore: reduced number of supported terraform versions in tests.

--- scripts/get-version-matrix.sh
+++ scripts/get-version-matrix.sh
@@ -6,5 +6,5 @@ function get_latest_version() {
             sort -V -r | head -1
 }
 
-echo terraform_versions="[$(get_latest_version v0.12), $(get_latest_version v0.13), $(get_latest_version v0.14), $(get_latest_version v0.15), $(get_latest_version v1.0), $(get_latest_version v1.1), $(get_latest_version v1.2), $(get_latest_version v1.3), $(get_latest_version v1.4), $(get_latest_version v1.5)]" >> $GITHUB_OUTPUT
+echo terraform_versions="[$(get_latest_version v0.15), $(get_latest_version v1.2), $(get_latest_version v1.3), $(get_latest_version v1.4), $(get_latest_version v1.5)]" >> $GITHUB_OUTPUT
 echo k8s_versions='["1.28.0", "1.27.3", "1.26.6", "1.25.11", "1.24.15", "1.23.17", "1.22.17"]' >> $GITHUB_OUTPUT
