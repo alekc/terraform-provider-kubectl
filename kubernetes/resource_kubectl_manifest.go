06b3f743d1763f3898daecb2ef78aaba4bc1571b
Alexander Chernov <alexander@chernov.it>
2024-03-16 18:12:46 +0100

    changed yaml_incluster structure and added additional debug message (#112)

--- kubernetes/resource_kubectl_manifest.go
+++ kubernetes/resource_kubectl_manifest.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"github.com/sergi/go-diff/diffmatchpatch"
 	"k8s.io/cli-runtime/pkg/genericiooptions"
 	k8sdelete "k8s.io/kubectl/pkg/cmd/delete"
 	"log"
@@ -303,7 +304,11 @@ metadata:
 			stateYaml := d.Get("yaml_incluster").(string)
 			liveStateYaml := d.Get("live_manifest_incluster").(string)
 			if stateYaml != liveStateYaml {
-				log.Printf("[TRACE] DETECTED YAML STATE %s vs %s", stateYaml, liveStateYaml)
+				log.Printf("[TRACE] DETECTED YAML STATE DIFFERENCE %s vs %s", stateYaml, liveStateYaml)
+				dmp := diffmatchpatch.New()
+				patches := dmp.PatchMake(stateYaml, liveStateYaml)
+				patchText := dmp.PatchToText(patches)
+				log.Printf("[DEBUG] DETECTED YAML INCLUSTER STATE DIFFERENCE. Patch diff: %s", patchText)
 				_ = d.SetNewComputed("yaml_incluster")
 			}
 
@@ -1010,9 +1015,7 @@ func getLiveManifestFingerprint(d *schema.ResourceData, userProvided *yaml.Manif
 	if hasIgnoreFields {
 		ignoreFields = expandStringList(ignoreFieldsRaw.([]interface{}))
 	}
-
-	fields := getLiveManifestFields_WithIgnoredFields(ignoreFields, userProvided, liveManifest)
-	return getFingerprint(fields)
+	return getLiveManifestFields_WithIgnoredFields(ignoreFields, userProvided, liveManifest)
 }
 
 func getFingerprint(s string) string {
@@ -1065,10 +1068,10 @@ func getLiveManifestFields_WithIgnoredFields(ignoredFields []string, userProvide
 		if _, exists := flattenedLive[userKey]; exists {
 			userKeys = append(userKeys, userKey)
 			normalizedLiveValue := strings.TrimSpace(flattenedLive[userKey])
-			flattenedUser[userKey] = normalizedLiveValue
 			if normalizedUserValue != normalizedLiveValue {
 				log.Printf("[TRACE] yaml drift detected in %s for %s, was: %s now: %s", userProvided.GetSelfLink(), userKey, normalizedUserValue, normalizedLiveValue)
 			}
+			flattenedUser[userKey] = getFingerprint(normalizedLiveValue)
 		} else {
 			if normalizedUserValue != "" {
 				log.Printf("[TRACE] yaml drift detected in %s for %s, was %s now blank", userProvided.GetSelfLink(), userKey, normalizedUserValue)
@@ -1077,12 +1080,12 @@ func getLiveManifestFields_WithIgnoredFields(ignoredFields []string, userProvide
 	}
 
 	sort.Strings(userKeys)
-	returnedValues := []string{}
+	var returnedValues []string
 	for _, k := range userKeys {
 		returnedValues = append(returnedValues, fmt.Sprintf("%s=%s", k, flattenedUser[k]))
 	}
 
-	return strings.Join(returnedValues, ",")
+	return strings.Join(returnedValues, "\n")
 }
 
 var kubernetesControlFields = []string{
