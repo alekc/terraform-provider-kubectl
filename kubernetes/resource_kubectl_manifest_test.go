b0d155888081d032ed1b89b31f45b5a5a321e58a
Alexander Chernov <alexander@chernov.it>
2026-05-18 10:07:08 +0100

    test: switch nginx fixtures to registry.k8s.io/e2e-test-images/nginx
    Docker Hub anonymous pulls from shared GitHub-runner IPs hit
    rate-limit / TooManyRequests on this branch's smoke job, leaving the
    test Pod stuck in ImagePullBackOff for the full 10-minute wait_for
    deadline. registry.k8s.io is hosted by the Kubernetes project and
    has no anonymous rate limits.
    
    `registry.k8s.io/e2e-test-images/nginx:1.28.0-1` is the upstream
    e2e test image (25 MB, runs `nginx -g daemon off;`, exposes port
    80). Drop-in for the existing readinessProbe httpGet on port 80;
    no test-config changes required beyond the image reference.
    
    Verified locally against kind k8s 1.34:
    - TestAccKubectl_WaitForConditions: 12.6s
    - TestAccKubectl_WaitForFieldAndCondition: 12.7s

--- kubernetes/resource_kubectl_manifest_test.go
+++ kubernetes/resource_kubectl_manifest_test.go
@@ -52,7 +52,7 @@ metadata:
 spec:
   containers:
   - name: nginx
-    image: nginx:1.27.2
+    image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
     readinessProbe:
       httpGet:
         path: "/"
@@ -99,7 +99,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.2
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -149,7 +149,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.2
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -199,7 +199,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.2
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -248,7 +248,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.2
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -298,7 +298,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.2
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -349,7 +349,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.2
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -387,7 +387,7 @@ metadata:
 spec:
   containers:
   - name: nginx
-    image: nginx:1.27.2
+    image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
     readinessProbe:
       httpGet:
         path: "/"
@@ -555,7 +555,7 @@ metadata:
 spec:
   containers:
   - name: nginx
-    image: nginx:1.27.2
+    image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
     readinessProbe:
       httpGet:
         path: "/"
@@ -601,7 +601,7 @@ metadata:
 spec:
   containers:
   - name: nginx
-    image: nginx:1.27.2
+    image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
     readinessProbe:
       httpGet:
         path: "/"
@@ -660,7 +660,7 @@ metadata:
 spec:
   containers:
   - name: nginx
-    image: nginx:1.27.2
+    image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
     readinessProbe:
       httpGet:
         path: "/"
@@ -708,7 +708,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.0
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -748,7 +748,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.2
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -800,7 +800,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.0
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
@@ -840,7 +840,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginx:1.27.2
+          image: registry.k8s.io/e2e-test-images/nginx:1.28.0-1
           ports:
             - containerPort: 80
           readinessProbe:
